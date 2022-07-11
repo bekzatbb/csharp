9c2f96bdcfa967788a018b196de8800976e9c321
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-07-11 06:40:20 +0600

    csharp

---
+++ 001/app.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+	class MyClass
+	{
+		static void Main(string[] args)
+		{
+			System.Console.WriteLine("Hello, ");
+			Console.WriteLine("World!");
+
+			Console.ReadLine();
+		}
+	}
+}
